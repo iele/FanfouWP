c6b2191f9294fb96af4041e527bdeb28e7bb8538
iele <melephas@outlook.com>
2014-04-16 15:53:41 +0800

    Working for FanfouWP 1.0.1

--- FanfouWP/ItemControls/ValueConverter/NullToVisibilityConverter.cs
+++ FanfouWP/ItemControls/ValueConverter/NullToVisibilityConverter.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace FanfouWP.ItemControls.ValueConverter
 {
-    public sealed class SourceToTextConverter : IValueConverter
+    public sealed class NullToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo language)
         {
-            return Regex.Replace((string)value, "<[^>]*>", "");
+            if (value == null || value as string == "")
+                return Visibility.Collapsed;
+            else
+                return Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo language)
--- FanfouWP/ItemControls/ValueConverter/RetweetToNameConverter.cs
+++ FanfouWP/ItemControls/ValueConverter/RetweetToNameConverter.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace FanfouWP.ItemControls.ValueConverter
 {
-    public sealed class SourceToTextConverter : IValueConverter
+    public sealed class RetweetToNameConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo language)
         {
-            return Regex.Replace((string)value, "<[^>]*>", "");
+
+            return "转自" + value as string;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo language)
